1095362c03130de1b1edcf9027dad3e4664233c4
travis-ci <travis@travis-ci.org>
2016-05-28 17:18:20 +0000

    Updated documentation on master for d3bc4d600592d7fe8919e6b935bc5f7d2cc0fe4c

--- master/implementors/core/ops/trait.DerefMut.js
+++ master/implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['shrust'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='shrust/struct.Shell.html' title='shrust::Shell'>Shell</a>&lt;T&gt;",];
+implementors['shrust'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='shrust/struct.Shell.html' title='shrust::Shell'>Shell</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
