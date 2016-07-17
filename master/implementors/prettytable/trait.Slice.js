2ff39567c7b5434475babde618fa03a54ca8c657
travis-ci <travis@travis-ci.org>
2016-07-17 11:40:36 +0000

    Updated documentation on master for 425ead66ea82ad132a8a6e786dc2d2cc097945c0

--- master/implementors/prettytable/trait.Slice.js
+++ master/implementors/prettytable/trait.Slice.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];
+implementors["prettytable"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/std/io/trait.Read.js
+++ master/implementors/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['shrust'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='shrust/struct.ShellIO.html' title='shrust::ShellIO'>ShellIO</a>",];
+implementors["shrust"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='shrust/struct.ShellIO.html' title='shrust::ShellIO'>ShellIO</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
