7096ebaddae06d8e4b0848c0bc7c0abf1fb7ce83
travis-ci <travis@travis-ci.org>
2016-08-29 08:38:17 +0000

    Updated documentation for release v0.0.3

---
+++ v0.0.3/shrust/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ExecError","Command execution error"]],"struct":[["History","Wrap the command history from a shell. It has a maximum capacity, and when max capacity is reached, less recent command is removed from history"],["Shell","A shell"],["ShellIO","Input / Output for shell execution"]],"type":[["ExecResult","Result from command execution"]]});
