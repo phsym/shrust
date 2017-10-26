6ef48a3f7823234dce773f40c4c2a59a1b18d8e1
travis-ci <travis@travis-ci.org>
2017-10-26 17:23:00 +0000

    Updated documentation on master for 9c856b761af038e0d78c289832c583aa6afd3962

--- master/implementors/std/io/trait.Write.js
+++ master/implementors/std/io/trait.Write.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["libc"] = [];
 implementors["shrust"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"shrust/struct.ShellIO.html\" title=\"struct shrust::ShellIO\">ShellIO</a>",];
 implementors["term"] = ["impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"term/terminfo/struct.TerminfoTerminal.html\" title=\"struct term::terminfo::TerminfoTerminal\">TerminfoTerminal</a>&lt;T&gt;",];
 
