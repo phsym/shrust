5170ea605ab7eb6df1292eb8a4cab9024a0ff22f
travis-ci <travis@travis-ci.org>
2018-05-26 17:21:42 +0000

    Updated documentation on master for 9c856b761af038e0d78c289832c583aa6afd3962

--- master/implementors/core/ops/deref/trait.DerefMut.js
+++ master/implementors/core/ops/deref/trait.DerefMut.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
-implementors["csv"] = ["impl&lt;R, I&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"csv/index/struct.Indexed.html\" title=\"struct csv::index::Indexed\">Indexed</a>&lt;R, I&gt;",];
-implementors["shrust"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"shrust/struct.Shell.html\" title=\"struct shrust::Shell\">Shell</a>&lt;T&gt;",];
+implementors["csv"] = [{text:"impl&lt;R, I&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"csv/index/struct.Indexed.html\" title=\"struct csv::index::Indexed\">Indexed</a>&lt;R, I&gt;",synthetic:false,types:["csv::index::Indexed"]},];
+implementors["shrust"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"shrust/struct.Shell.html\" title=\"struct shrust::Shell\">Shell</a>&lt;T&gt;",synthetic:false,types:["shrust::Shell"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
