5b96c99a2d51a9744636dd801c60f87b2fe93ebe
travis-ci <travis@travis-ci.org>
2018-06-24 09:53:55 +0000

    Updated documentation on master for e407cd668021508d220239c85343f3230f4ee916

---
+++ master/aliases.js
@@ -0,0 +1,13 @@
+var ALIASES = {};
+ALIASES['atty'] = {};
+ALIASES['byteorder'] = {};
+ALIASES['csv'] = {};
+ALIASES['encode_unicode'] = {};
+ALIASES['lazy_static'] = {};
+ALIASES['libc'] = {};
+ALIASES['memchr'] = {};
+ALIASES['prettytable'] = {};
+ALIASES['rustc_serialize'] = {};
+ALIASES['shrust'] = {};
+ALIASES['term'] = {};
+ALIASES['unicode_width'] = {};
