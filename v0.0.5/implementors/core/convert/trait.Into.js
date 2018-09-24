198f33164c77c16100b8a771d7bb8d3a5f2f8d47
travis-ci <travis@travis-ci.org>
2018-09-24 09:28:31 +0000

    Updated documentation for release v0.0.5

---
+++ v0.0.5/implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["prettytable"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"prettytable/format/struct.TableFormat.html\" title=\"struct prettytable::format::TableFormat\">TableFormat</a>&gt; for <a class=\"struct\" href=\"prettytable/format/struct.FormatBuilder.html\" title=\"struct prettytable::format::FormatBuilder\">FormatBuilder</a>",synthetic:false,types:["prettytable::format::FormatBuilder"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
