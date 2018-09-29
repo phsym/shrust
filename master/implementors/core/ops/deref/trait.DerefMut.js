933929f1a7bceae18dbd0abd7e3c90bf916593e8
travis-ci <travis@travis-ci.org>
2018-09-29 07:40:14 +0000

    Updated documentation on master for ec69b0eebc8e027ade330a430dc8ea3370708c2f

--- master/implementors/core/ops/deref/trait.DerefMut.js
+++ master/implementors/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["csv"] = [{text:"impl&lt;R, I&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"csv/index/struct.Indexed.html\" title=\"struct csv::index::Indexed\">Indexed</a>&lt;R, I&gt;",synthetic:false,types:["csv::index::Indexed"]},];
 implementors["shrust"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"shrust/struct.Shell.html\" title=\"struct shrust::Shell\">Shell</a>&lt;T&gt;",synthetic:false,types:["shrust::Shell"]},];
 
             if (window.register_implementors) {
